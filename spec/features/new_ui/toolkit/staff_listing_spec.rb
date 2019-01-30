fc3223350cc861c7aaeeac00e47dc6f12d7531e6
SAMUEL HAILE <samishken@gmail.com>
2019-01-30 17:44:42 -0500

    Role change test added to staff_listing test file

--- spec/features/new_ui/toolkit/staff_listing_spec.rb
+++ spec/features/new_ui/toolkit/staff_listing_spec.rb
@@ -261,6 +261,33 @@ describe "Staff Listing", js:true do
           find("button", text: 'Save').click
         end
       end
+      # Test Role change (from teacher to counselor)
+      assert_equal("/users/staff_listing", current_path)
+      within("#page-content") do
+        within("tr#user_#{@teacher.id}") do
+          page.should have_content("teacher")
+          page.should have_css("i.fa-edit")
+          page.should have_css("a[data-url='/users/#{@teacher.id}/edit.js'] i.fa-edit")
+          page.find("a[data-url='/users/#{@teacher.id}/edit.js']").click
+        end
+        if [:school_administrator, :system_administrator].include?(role)
+          find("input[name='user[teacher]']").set(false)
+          find("input[name='user[counselor]']").set(true)
+          page.should have_css("button", text: 'Save')
+          find("button", text: 'Save').click
+        else # teacher editing
+          assert_equal(page.all("fieldset.role-field").count.should, 0)
+          page.should_not have_css('fieldset#role-sch-admin')
+          page.should_not have_css('fieldset#role-teach')
+          page.should_not have_css('fieldset#role-couns')
+        end
+        # Ensure role changed correctly
+        assert_equal("/users/staff_listing", current_path)
+        within("tr#user_#{@teacher.id}") do
+          page.should have_content("counselor")
+        end
+      end
+
       assert_equal("/users/staff_listing", current_path)
       within("#page-content table #user_#{@teacher.id}") do
         page.should have_css('.user-first-name', 'Changed First Name')
@@ -277,6 +304,8 @@ describe "Staff Listing", js:true do
         end
       end
     end
+
+
     # teachers and counselors cannot edit other user's information
     # researchers cannot edit any user's information
     if [:teacher, :counselor, :researcher].include?(role)
