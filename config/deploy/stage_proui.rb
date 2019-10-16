084c10e1da9dbcc3278728148fe713e471d3b044
esopp <emily@Tonys-MacBook-Pro-3.local>
2019-10-16 15:16:37 -0400

    update config/deploy files for egstage, stage_proui, and usstage

--- config/deploy/stage_proui.rb
+++ config/deploy/stage_proui.rb
@@ -1,7 +1,7 @@
 set :stage, :stage_proui
-server 'staging.parloproject.org', roles: %w{web app db}
+server 'proui.parloproject.org', roles: %w{web app db}
 set :rvm_ruby_version, '2.2.9'
 set :deploy_to, '/web/parlo-tracker/proui'
 set :rails_env, 'staging'
-# set :bundle_dir, "/usr/local/rvm/gems/ruby-1.9.3-p550"
-set :bundle_dir, "~/.rvm/bin/gems/ruby-2.2.9-p480"
+# set :bundle_dir seems to be ignored. Currently goes to ~/.rvm/gems/ruby-2.5.6/bin/bundler
+# set :bundle_dir, "~/.rvm/bin/gems/ruby-2.2.9-p480"
--- config/deploy/usstage.rb
+++ config/deploy/usstage.rb
@@ -3,4 +3,5 @@ server 'usstage.parloproject.org', roles: %w{web app db}
 set :rvm_ruby_version, '2.5.6'
 set :deploy_to, '/web/parlo-tracker/usstage'
 set :rails_env, 'usstage'
-set :bundle_dir, "~/.rvm/bin/gems/ruby-2.2.9-p480"
+# set :bundle_dir seems to be ignored. Currently goes to ~/.rvm/gems/ruby-2.5.6/bin/bundler
+#set :bundle_dir, "~/.rvm/bin/gems/ruby-2.2.9-p480"
