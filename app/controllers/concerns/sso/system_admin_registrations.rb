1e1af9719067d19a4ced0be56d8a0c4f559357cc
rapkat10 <aminrapkat@gmail.com>
2020-07-21 10:35:15 -0600

    change ruby version

--- app/controllers/concerns/sso/system_admin_registrations.rb
+++ app/controllers/concerns/sso/system_admin_registrations.rb
@@ -3,14 +3,6 @@ module Sso::SystemAdminRegistrations
 
   def create_system_user
     super do |user|
-      # payload = {email: user.email, password: user.temporary_password}
-      #TODO: encrypt params in token
-      # What I did: removed second arg in block and updated the password field.
-      # User is now created in SSO and can verify temporary_password and log in.
-      # Things to work on next: 
-        # 1. when user password is reset or updated, it should also update SSO!
-        # 2. permissions in SSO!
-        # 3. add this logic to all controllers that create users!
       body = {
         user:{
           email: user.email, 
